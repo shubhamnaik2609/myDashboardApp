19eed27de526efd96820235c00e31299bdad7e75
Shubham Naik <shubhamnaik2609>
2025-02-12 17:50:20 -0500

    Setting the app link

---
+++ myDashboardApp/src/pages/categories/create.tsx
@@ -0,0 +1,30 @@
+import { Create, useForm } from "@refinedev/antd";
+import { Form, Input } from "antd";
+import React from "react";
+    import { CATEGORY_CREATE_MUTATION } from './queries'
+
+export const CategoryCreate = () => {
+    const { formProps, saveButtonProps } = useForm({
+        meta: {
+            gqlMutation: CATEGORY_CREATE_MUTATION,
+        },
+    });
+
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label={"Title"}
+                    name={["title"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
+};
---
+++ myDashboardApp/src/pages/categories/edit.tsx
@@ -0,0 +1,31 @@
+import { Edit, useForm } from "@refinedev/antd";
+import { Form, Input } from "antd";
+import React from "react";
+    import { CATEGORY_EDIT_MUTATION } from './queries'
+
+
+export const CategoryEdit = () => {
+    const { formProps, saveButtonProps } = useForm({
+        meta: {
+            gqlMutation: CATEGORY_EDIT_MUTATION,
+        },
+    });
+
+    return (
+        <Edit saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label={"Title"}
+                    name={["title"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+            </Form>
+        </Edit>
+    );
+};
